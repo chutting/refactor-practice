d22978c8114a3e78a0889ca0033b30671a294684
chutting <tingting.chu@thoughtworks.com>
2020-09-14 22:44:03 +0800

    [Chu] refactor: summary four methods to one method

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -1,5 +1,8 @@
 package com.twu.refactoring;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class NumberCruncher {
     private final int[] numbers;
 
@@ -7,34 +10,19 @@ public class NumberCruncher {
         this.numbers = numbers;
     }
 
-    public int countEven() {
+    public int count(String countType) {
         int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
+        if (countType.equals("even")) {
+            count = (int) Arrays.stream(numbers).filter(number -> number % 2 == 0).count();
         }
-        return count;
-    }
-
-    public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
+        if (countType.equals("odd")) {
+            count = (int) Arrays.stream(numbers).filter(number -> number % 2 == 1).count();
         }
-        return count;
-    }
-
-    public int countPositive() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number >= 0) count++;
+        if (countType.equals("positive")) {
+            count = (int) Arrays.stream(numbers).filter(number -> number >= 0).count();
         }
-        return count;
-    }
-
-    public int countNegative() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number < 0) count++;
+        if (countType.equals("negative")) {
+            count = (int) Arrays.stream(numbers).filter(number -> number < 0).count();
         }
         return count;
     }
--- 4/src/test/java/com/twu/refactoring/NumberCruncherTest.java
+++ 4/src/test/java/com/twu/refactoring/NumberCruncherTest.java
@@ -8,25 +8,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class NumberCruncherTest {
     @Test
     public void shouldCountEvenNumbers() {
-        int evens = new NumberCruncher(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11).countEven();
+        int evens = new NumberCruncher(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11).count("even");
         assertThat(evens, is(5));
     }
 
     @Test
     public void shouldCountOddNumbers() {
-        int odds = new NumberCruncher(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11).countOdd();
+        int odds = new NumberCruncher(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11).count("odd");
         assertThat(odds, is(6));
     }
 
     @Test
     public void shouldCountPositiveNumbers() {
-        int positives = new NumberCruncher(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4).countPositive();
+        int positives = new NumberCruncher(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4).count("positive");
         assertThat(positives, is(5));
     }
 
     @Test
     public void shouldCountNegativeNumbers() {
-        int negatives = new NumberCruncher(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4).countNegative();
+        int negatives = new NumberCruncher(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4).count("negative");
         assertThat(negatives, is(5));
     }
 }
